077d403a1ce37bbbb9aac548b9e22f764d3a576f
Evocator <74635324+Evocator@users.noreply.github.com>
2020-12-06 19:44:13 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use(require('./routes/index.js'))
+app.use(require('/routes/index.js'))
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/src', function(req, res){
