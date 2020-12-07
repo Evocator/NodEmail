c2d04903584131f7b8fa7486263d4ccb2f6552d6
Evocator <manuel@dudeagency.io>
2020-12-06 17:48:15 -0800

    Initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,48 @@
+const { Router } = require('express');
+const router = Router();
+
+const nodemailer = require('nodemailer');
+
+router.post('/send-email', async (req, res) => {
+    const { name, email, phone, message } = req.body;
+
+    contentHTML = `
+        <h1>User Information</h1>
+        <ul>
+            <li>Username: ${name}</li>
+            <li>User Email: ${email}</li>
+            <li>PhoneNumber: ${phone}</li>
+        </ul>
+        <p>${message}</p>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'xrodriguezvazquezx@gmail.com',
+            pass: 'megadeth18'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Manuel Server" <xrodriguezvazquezx@gmail.com>', // sender address,
+        to: 'xrodriguezvazquezx@gmail.com',
+        subject: 'Cotizacion',
+        // text: 'Hello World'
+        html: contentHTML
+    })
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    res.redirect('/success.html');
+});
+
+module.exports = router;
