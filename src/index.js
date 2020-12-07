dc1a645ad845603d1eaa82b051405504b1fdd7ec
Evocator <74635324+Evocator@users.noreply.github.com>
2020-12-06 20:09:26 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const app = express(); 
+const app = express();
 const path = require('path');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use(require('./routes/index.js'))
-app.use(express.static(path.join(__dirname,'public')));
+app.use(require('./routes/index'));
 
-app.get('/src', function(req, res) {
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.listen (3000,() => {
-    console.log('Servidor en puerto 3000');
-});
-
-
+app.listen(3000);
+console.log('Server on port 3000')
