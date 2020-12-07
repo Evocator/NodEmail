907479e6980d86b2be3fe975e53211bcb8d40604
Evocator <74635324+Evocator@users.noreply.github.com>
2020-12-06 19:57:15 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(require('./routes/index.js'))
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/NodEmail/', function(req, res) {
+app.get('/', function(req, res) {
   // do something here.
 });
 
